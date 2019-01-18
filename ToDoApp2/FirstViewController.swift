74e793cea8d9a4c4be80cada42aa4535e152fb9d
Kan Nakamura <kan.nakamura0711@gmail.com>
2019-01-18 19:57:25 +0900

    Initial Commit

---
+++ ToDoApp2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoApp2
+//
+//  Created by Kan Nakamura on 2019/01/18.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ToDoApp2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ToDoApp2
+//
+//  Created by Kan Nakamura on 2019/01/18.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
